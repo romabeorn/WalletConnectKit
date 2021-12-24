093b0397fe0023b1fe6f564c315819583305d18f
Roman Matveev <romabeorn@gmail.com>
2021-12-24 09:57:07 +0300

    Initial Commit

---
+++ WalletConnectKit/WalletConnectKit.h
@@ -0,0 +1,18 @@
+//
+//  WalletConnectKit.h
+//  WalletConnectKit
+//
+//  Created by Overcout on 24.12.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WalletConnectKit.
+FOUNDATION_EXPORT double WalletConnectKitVersionNumber;
+
+//! Project version string for WalletConnectKit.
+FOUNDATION_EXPORT const unsigned char WalletConnectKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WalletConnectKit/PublicHeader.h>
+
+
